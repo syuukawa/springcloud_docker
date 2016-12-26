a1c45c5d95d229cd20d8ca80b5eaaf2ec37a30e9
syuukawa <syuukawa@sina.cn>
2016-12-26 16:56:39 +0800

    org.slf4j.Logger;
    org.slf4j.Logger;

--- demo_layout/discovery/src/main/java/com/syuukawa/cloud/DisApplication.java
+++ demo_layout/discovery/src/main/java/com/syuukawa/cloud/DisApplication.java
@@ -1,5 +1,7 @@
 package com.syuukawa.cloud;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -7,9 +9,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class DisApplication {
-
+    private static final Logger log = LoggerFactory.getLogger(DisApplication.class);
     public static void main(String[] args) {
-
+        log.debug("main");
         SpringApplication.run(DisApplication.class, args);
     }
 }
